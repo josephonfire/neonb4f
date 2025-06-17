37a3e55e3f9fac7f44d8d58113c03ff6d4e475ef
fchinita98 <fchinita98@gmail.com>
2025-06-17 10:33:47 +0100

    updated JSON cards

--- src/app/top100a/page.jsx
+++ src/app/top100a/page.jsx
@@ -1,10 +1,19 @@
 
 import {Card} from '../components/Card'
+import data from '../data/history.json'
 
 export default function PageTop() {
   return (
     <div>
-      <Card img={"imagem_teste.jpg"} position={1} artistName={"Travis Scott"}/>
+      <div>
+        {data.map(DataMap => {
+          return(
+            <div>
+              <Card position={1} artistName={DataMap.master_metadata_album_artist_name}/>
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
 }
