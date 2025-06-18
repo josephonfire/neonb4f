378afc0b418603c98bc56c7a153a39f5457a050d
Ellie-Yeah <elisabeteff.69@gmail.com>
2025-06-18 11:46:32 +0100

    charts: Add DonutChart component and update PieChart

---
+++ src/app/artistpage/page.tsx
@@ -0,0 +1,12 @@
+//pag artista
+
+import React from 'react';
+import './globals.css'; // Import global styles
+import NavBar from '../app/components/NavBar.jsx';
+import ArtistInfoBar from '../app/components/ArtistInfoBar.jsx';
+import ArtistInfoBar2 from '../app/components/ArtistInfoBar2.jsx';
+import ArtistInfoBar3 from '../app/components/ArtistInfoBar3.jsx';
+import PieChart from '../app/components/PieChart.jsx';
+import DonnutChart from '../app/components/DonnutChart.jsx';
+import Card from '../app/components/Card.jsx';
+import MusicCard from '../app/components/MusicCard.jsx';
