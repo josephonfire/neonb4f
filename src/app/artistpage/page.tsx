6b309f5e837153092794957608e423ffdcaabc74
Ellie-Yeah <elisabeteff.69@gmail.com>
2025-06-18 14:13:44 +0100

    artistpage: remove unused assets and clean up code

--- src/app/artistpage/page.tsx
+++ src/app/artistpage/page.tsx
@@ -9,7 +9,10 @@ import ArtistInfoBar3 from '../components/ArtistInfoBar3.jsx';
 import PieChart from '../components/PieChart.jsx';
 import DonnutChart from '../components/DonnutChart.jsx';
 import MusicCard from '../components/MusicCard.jsx';
-
+// Import data and ensure it's typed correctly
+import data from '../data/history.json';
+import { useTopSongs } from '../hooks/hooks.jsx'
+import { useTopArtists, useTopArtists2 } from '../hooks/hooks.jsx'
 
 function App(props: { artistName: string; position: number }) {
     return (
@@ -18,7 +21,7 @@ function App(props: { artistName: string; position: number }) {
             <div>
                 <div>
                     <img className='artist-profile-photo' src="/imgs/spoti-singer-icon.svg" alt="artist profile photo" /></div>
-                <h1 className='artist-name'>fhgihdfksdi{props.artistName}</h1>
+                <h1 className='artist-name'>{props.artistName}</h1>
             </div>
             <div className='artist-position'>
                 <p>TOP #{props.position}</p>
@@ -30,10 +33,14 @@ function App(props: { artistName: string; position: number }) {
             <div className='artist-info-bar'>
                 <ArtistInfoBar />
                 <ArtistInfoBar2 />
-                <ArtistInfoBar3 /></div>
-            <div className='cards'>
+                <ArtistInfoBar3 />
+                </div>
 
-                <MusicCard />
+            <div className='cards'>
+            <h1 className='top-list'>TOP 20 Músicas</h1>
+            {Array.from({ length: 20 }).map((_, index) => (
+                    <MusicCard key={index} />
+                ))}
             </div>
 
 
